36d1027cfd370ff895616e128d931bd0f875e8b5
Palak0905 <pmaheshwari291@gmail.com>
2026-03-05 10:33:12 +0530

    Problem10:Multi-Level Cache System with Hash Tables

--- src/Week01and02.java
+++ src/Week01and02.java
@@ -1,41 +1,40 @@
 import java.util.*;
 
-class Transaction{
-    int id;
-    int amount;
-    String merchant;
-    String time;
-    Transaction(int id,int amount,String merchant,String time){
-        this.id=id;
-        this.amount=amount;
-        this.merchant=merchant;
-        this.time=time;
+class LRU<K,V> extends LinkedHashMap<K,V>{
+    int cap;
+    LRU(int cap){
+        super(cap,0.75f,true);
+        this.cap=cap;
+    }
+    protected boolean removeEldestEntry(Map.Entry<K,V> e){
+        return size()>cap;
     }
 }
 
 public class Week01and02 {
 
-    static List<Transaction> list=new ArrayList<>();
+    static LRU<String,String> L1=new LRU<>(10000);
+    static HashMap<String,String> L2=new HashMap<>();
+    static HashMap<String,String> L3=new HashMap<>();
 
-    static void add(Transaction t){
-        list.add(t);
-    }
-
-    static void twoSum(int target){
-        HashMap<Integer,Transaction> map=new HashMap<>();
-        for(Transaction t:list){
-            int c=target-t.amount;
-            if(map.containsKey(c)){
-                System.out.println(map.get(c).id+" "+t.id);
-            }
-            map.put(t.amount,t);
+    static String getVideo(String id){
+        if(L1.containsKey(id)){
+            return "L1 HIT";
         }
+        if(L2.containsKey(id)){
+            L1.put(id,L2.get(id));
+            return "L2 HIT";
+        }
+        if(L3.containsKey(id)){
+            L2.put(id,L3.get(id));
+            return "L3 HIT";
+        }
+        return "NOT FOUND";
     }
 
     public static void main(String[] args){
-        add(new Transaction(1,500,"StoreA","10:00"));
-        add(new Transaction(2,300,"StoreB","10:15"));
-        add(new Transaction(3,200,"StoreC","10:30"));
-        twoSum(500);
+        L3.put("video_123","data");
+        System.out.println(getVideo("video_123"));
+        System.out.println(getVideo("video_123"));
     }
 }
