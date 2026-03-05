65c87f8f3c06812f96b6588a40eb38c54c8f4a49
Palak0905 <pmaheshwari291@gmail.com>
2026-03-05 10:18:26 +0530

    Problem4:Plagiarism Detection System

--- src/Week01and02.java
+++ src/Week01and02.java
@@ -1,42 +1,44 @@
 import java.util.*;
 
-class DNSEntry{
-    String ip;
-    long expiry;
-    DNSEntry(String ip,long ttl){
-        this.ip=ip;
-        this.expiry=System.currentTimeMillis()+ttl;
-    }
-}
-
 public class Week01and02 {
 
-    static HashMap<String,DNSEntry> cache=new HashMap<>();
-    static int hits=0;
-    static int miss=0;
+    static HashMap<String,Set<String>> map=new HashMap<>();
 
-    static String resolve(String domain){
-        DNSEntry e=cache.get(domain);
-        long now=System.currentTimeMillis();
-        if(e!=null && e.expiry>now){
-            hits++;
-            return "Cache HIT "+e.ip;
+    static List<String> ngrams(String text,int n){
+        String[] w=text.split(" ");
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<=w.length-n;i++){
+            String g="";
+            for(int j=0;j<n;j++) g+=w[i+j]+" ";
+            list.add(g.trim());
         }
-        miss++;
-        String ip="172.217."+new Random().nextInt(200)+"."+new Random().nextInt(200);
-        cache.put(domain,new DNSEntry(ip,300000));
-        return "Cache MISS "+ip;
+        return list;
     }
 
-    static void stats(){
-        int total=hits+miss;
-        double rate=(total==0)?0:(hits*100.0/total);
-        System.out.println("Hit Rate "+rate+"%");
+    static void addDoc(String id,String text){
+        for(String g:ngrams(text,3)){
+            map.putIfAbsent(g,new HashSet<>());
+            map.get(g).add(id);
+        }
+    }
+
+    static void check(String text){
+        Map<String,Integer> score=new HashMap<>();
+        for(String g:ngrams(text,3)){
+            if(map.containsKey(g)){
+                for(String d:map.get(g)){
+                    score.put(d,score.getOrDefault(d,0)+1);
+                }
+            }
+        }
+        for(String k:score.keySet()){
+            System.out.println(k+" "+score.get(k));
+        }
     }
 
     public static void main(String[] args){
-        System.out.println(resolve("google.com"));
-        System.out.println(resolve("google.com"));
-        stats();
+        addDoc("essay1","data science is very interesting and useful");
+        addDoc("essay2","machine learning and data science are related");
+        check("data science is useful");
     }
 }
